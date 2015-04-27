62e89b762c0de51357cce49e27dc0584ea2f220f
Shing Lyu <shing.lyu@gmail.com>
2015-04-27 12:14:15 +0800

    Debug env var

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 reply=$(curl -X POST -H "Content-Type: application/json" --data '{"caseversion":"/api/v1/caseversion/68161/", "instruction":"Open phone app", "expected":"Phone app opened", "number":1}' https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY)
 #reply=$(curl -X GET -H "Content-Type: application/json" https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY)
+echo $MTUSER
 echo $reply
