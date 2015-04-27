b8110a812e5431e6563529923264ed9994db1fce
Shing Lyu <shing.lyu@gmail.com>
2015-04-27 12:10:46 +0800

    POST in push

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-#curl -X POST -H "Content-Type: application/json" --data '{"caseversion":"/api/v1/caseversion/68161/", "instruction":"Open phone app", "expected":"Phone app opened", "number":1}' https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY
-reply=$(curl -X GET -H "Content-Type: application/json" https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY)
+reply=$(curl -X POST -H "Content-Type: application/json" --data '{"caseversion":"/api/v1/caseversion/68161/", "instruction":"Open phone app", "expected":"Phone app opened", "number":1}' https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY)
+#reply=$(curl -X GET -H "Content-Type: application/json" https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY)
 echo $reply
