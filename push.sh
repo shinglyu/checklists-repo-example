5ca0ecd502299d66cce3e1bc81a3f67e15901a84
Shing Lyu <shing.lyu@gmail.com>
2015-04-27 11:53:11 +0800

    Travis config

---
+++ push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H "Content-Type: application/json" --data '{"caseversion":"/api/v1/caseversion/68161/", "instruction":"Open phone app", "expected":"Phone app opened", "number":1}' https://moztrap-dev.allizom.org/api/v1/casestep/\?username\=$MTUSER\&api_key\=$APIKEY
