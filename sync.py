275586f60de816fe294fc512b4d07f9233d730be
shako <sho@mozilla.com>
2015-05-04 10:56:33 +0800

    revert sync.py to normal state

--- sync.py
+++ sync.py
@@ -6,14 +6,11 @@ import os
 tmpfile="tmp.txt"
 
 #FIXME: hardcoded feature file name
-cmd = "%s %s -r %s " % ('./moztrap_integration/cucumber-js/bin/cucumber.js', 'fxos.rocketbar.feature',
-                            './moztrap_integration/step_definitions/moztrap.steps.js')
-print cmd
+cmd = "%s %s -r %s > %s" % ('./moztrap_integration/cucumber-js/bin/cucumber.js', 'fxos.rocketbar.feature',
+                            './moztrap_integration/step_definitions/moztrap.steps.js', tmpfile)
 os.system(cmd)
 #TODO: detect for changed/added/delete case only and ignore the rest
 
-tmp = open(tmpfile, "r")
 #FIXME: hardcoded username/apikey
 mtapi.mtorigin = "https://moztrap-dev.allizom.org"
-print tmp.readlines()
 mtapi.create(tmpfile, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
